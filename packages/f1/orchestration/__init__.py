b4cd5e96b4bbcbd994028c4d9ca65cd71489dfca
Lemonnier Hugo <hugo.lemonnier01@gmail.com>
2026-07-13 00:20:12 +0200

    refactor(f1): expose non-live validation APIs
    - export event-block promotion diagnostics
    - export optional model runtime inspection helpers

--- packages/f1/orchestration/__init__.py
+++ packages/f1/orchestration/__init__.py
@@ -17,21 +17,47 @@ from .model_promotion import (
     live_strategy_promotion_config,
     ultimate_lap_time_promotion_config,
 )
+from .model_runtime import (
+    OptionalModelRuntime,
+    f1_model_runtime_doctor,
+    inspect_optional_model_runtime,
+)
+from .non_live_validation import (
+    EventError,
+    NonLivePromotionDecision,
+    PairedEventDiagnostics,
+    evaluate_best_lap_promotion,
+    evaluate_qualifying_promotion,
+    evaluate_race_promotion,
+    paired_event_diagnostics,
+    validate_event_partitions,
+)
 from .scenarios import F1Scenario
 
 __all__ = [
     "F1Scenario",
     "F1ModelRegistryEntry",
+    "EventError",
     "MetricComparison",
     "ModelRegistry",
+    "NonLivePromotionDecision",
+    "OptionalModelRuntime",
+    "PairedEventDiagnostics",
     "PromotionEvidence",
     "PromotionDecision",
     "PromotionGateConfig",
     "RegistryPromotionEvaluation",
     "RegistryPromotionResult",
     "evaluate_model_promotion",
+    "evaluate_best_lap_promotion",
+    "evaluate_qualifying_promotion",
+    "evaluate_race_promotion",
+    "f1_model_runtime_doctor",
+    "inspect_optional_model_runtime",
     "live_strategy_promotion_config",
+    "paired_event_diagnostics",
     "promotion_config_for_family",
     "registry_entry_from_profile",
     "ultimate_lap_time_promotion_config",
+    "validate_event_partitions",
 ]
